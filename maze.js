f9d156defdc693a099cdd5af9f1637c1a6c5fbb1
Dukie <44005207+legendarykydd@users.noreply.github.com>
2018-10-22 12:12:40 -0500

    Update maze.js
    Exercise 2 completed

--- maze.js
+++ maze.js
@@ -9,5 +9,21 @@ window.onload = function() {
 
 });
 
+	let boundaries = document.querySelectorAll(".boundary");
+	let index1 = -1;
+
+	for (var i = 0; i < boundaries.length; i++) {
+		if (boundaries [1].className.indexOf("example") != -1) {
+			index1 = i;
+		} else {
+			boundaries[i].addEventListener("mouseover", function(){
+				document.getElementById("status").textContent = "You Lose"
+				for (var i = 0; i <boundaries.length; i++) {
+					boundaries[i].className = "boundary youlose"
+				}
+			});
+		}
+	}
+
 
 }
