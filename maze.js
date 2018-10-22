da7b2c0da7cff5b11d64ece68810e6c6bc81ee5d
Dukie <44005207+legendarykydd@users.noreply.github.com>
2018-10-22 12:09:54 -0500

    Update maze.js
    Exercise one completed

---
+++ maze.js
@@ -0,0 +1,13 @@
+
+window.onload = function() {
+
+
+	let boundary = document.getElementById("boundary1");
+
+	boundary.addEventListener("mouseover", function(){
+		this.className = "boundary youlose";
+
+});
+
+
+}
